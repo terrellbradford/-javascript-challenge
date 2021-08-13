0c2f1b4742deb8298ae7f658da541659c1f389aa
Terrell Bradford <terrell.bradford@gmail.com>
2021-08-13 17:44:09 -0400

    assignment complete attempt athe level-2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -6,13 +6,10 @@ var tbody = d3.select("tbody");
 var filter = d3.select("#filter-btn");
 var clear = d3.select("#clear-btn");
 var city_filter = d3.select("#city-filter-btn");
-var city_clear = d3.select("#city-clear-btn");
 var state_filter = d3.select("#state-filter-btn");
-var state_clear = d3.select("#state-clear-btn");
 var country_filter = d3.select("#country-filter-btn");
-var country_clear = d3.select("#country-clear-btn");
 var shape_filter = d3.select("#shape-filter-btn");
-var shape_clear = d3.select("#shape-clear-btn");
+
 
 // Use Arrow Functions to update each cell's text key and value
 tableData.forEach((ufoSighting) => {
@@ -107,20 +104,113 @@ city_filter.on("click", function() {
   });
 });
 
-city_clear.on("click", function() {
-    tbody.html("");
+clear.on("click", function() {
+  tbody.html("");
 
-    tableData.forEach((ufoSighting) => {
-        console.log(ufoSighting);
-        // append a table row for each value
-        var row = tbody.append("tr");
-        // adding objects
-        Object.entries(ufoSighting).forEach(([key, value]) => {
-          console.log(key, value);
-          // append a cell to the row for each value
-          var cell = row.append("td");
-          cell.text(value);
-        });
+  tableData.forEach((ufoSighting) => {
+      console.log(ufoSighting);
+      // append a table row for each value
+      var row = tbody.append("tr");
+      // adding objects
+      Object.entries(ufoSighting).forEach(([key, value]) => {
+        console.log(key, value);
+        // append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
       });
+    });
 
 });
+
+// Create a filter for new data
+state_filter.on("click", function() {
+
+tbody.html("");
+
+// input element
+var inputElement = d3.select("#state");
+
+// user input
+var inputValue = inputElement.property("value");
+console.log(inputValue);
+
+// Filter Data with datetime equal to user input
+var filteredData = tableData.filter(num_of_sightings => num_of_sightings.state === inputValue);
+console.log(filteredData);
+
+// Use Arrow Functions to update each cell's text key and value with filtered data
+filteredData.forEach(num_of_sightings => {
+  console.log(num_of_sightings);
+  // append a table row for each value
+  var next_row = tbody.append("tr");
+  // adding key value pair
+  Object.entries(num_of_sightings).forEach(([key, value]) => {
+    console.log(key, value);
+    // append a cell to the row for each value
+    var cell = next_row.append("td");
+    cell.text(value);
+  });
+});
+});
+
+// Create a filter for new data
+country_filter.on("click", function() {
+
+  tbody.html("");
+  
+  // input element
+  var inputElement = d3.select("#country");
+  
+  // user input
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  
+  // Filter Data with datetime equal to user input
+  var filteredData = tableData.filter(num_of_sightings => num_of_sightings.country === inputValue);
+  console.log(filteredData);
+  
+  // Use Arrow Functions to update each cell's text key and value with filtered data
+  filteredData.forEach(num_of_sightings => {
+    console.log(num_of_sightings);
+    // append a table row for each value
+    var next_row = tbody.append("tr");
+    // adding key value pair
+    Object.entries(num_of_sightings).forEach(([key, value]) => {
+      console.log(key, value);
+      // append a cell to the row for each value
+      var cell = next_row.append("td");
+      cell.text(value);
+    });
+  });
+  });
+
+  // Create a filter for new data
+shape_filter.on("click", function() {
+
+  tbody.html("");
+  
+  // input element
+  var inputElement = d3.select("#shape");
+  
+  // user input
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
+  
+  // Filter Data with datetime equal to user input
+  var filteredData = tableData.filter(num_of_sightings => num_of_sightings.shape === inputValue);
+  console.log(filteredData);
+  
+  // Use Arrow Functions to update each cell's text key and value with filtered data
+  filteredData.forEach(num_of_sightings => {
+    console.log(num_of_sightings);
+    // append a table row for each value
+    var next_row = tbody.append("tr");
+    // adding key value pair
+    Object.entries(num_of_sightings).forEach(([key, value]) => {
+      console.log(key, value);
+      // append a cell to the row for each value
+      var cell = next_row.append("td");
+      cell.text(value);
+    });
+  });
+  });
